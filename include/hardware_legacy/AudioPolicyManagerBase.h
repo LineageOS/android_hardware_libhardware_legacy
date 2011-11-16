4ced998d6e18c449c3ea16da606086a46fe9c66a
Ravi Kumar Alamanda <ralama@codeaurora.org>
2011-11-16 13:20:51 -0800

    hardware/libhardware_legacy: Add interface function for LPA and FM
    - New interface function prototypes are added for LPA and FM
      features
    
      * add JB API compatibility to LPA's openOutputSession
      * use JB-style QCOM_FM_ENABLED and QCOM_TUNNEL_LPA_ENABLED
      * whitespace and minor clean ups
    
    Change-Id: I1e7cb94668e2999b055acfa0c31652d5d1e719c1

--- include/hardware_legacy/AudioPolicyManagerBase.h
+++ include/hardware_legacy/AudioPolicyManagerBase.h
@@ -355,6 +355,9 @@ protected:
         virtual float computeVolume(int stream, int index, audio_io_handle_t output, audio_devices_t device);
 
         // check that volume change is permitted, compute and send new volume to audio hardware
+#ifdef QCOM_TUNNEL_LPA_ENABLED
+        virtual
+#endif
         status_t checkAndSetVolume(int stream, int index, audio_io_handle_t output, audio_devices_t device, int delayMs = 0, bool force = false);
 
         // apply all stream volumes to the specified output and device
@@ -368,6 +371,9 @@ protected:
                              audio_devices_t device = (audio_devices_t)0);
 
         // Mute or unmute the stream on the specified output
+#ifdef QCOM_TUNNEL_LPA_ENABLED
+        virtual
+#endif
         void setStreamMute(int stream,
                            bool on,
                            audio_io_handle_t output,
