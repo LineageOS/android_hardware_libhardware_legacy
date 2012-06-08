ceb4b4c43f67b37de129e1cb8109f8db6930423f
XpLoDWilD <xplodgui@gmail.com>
2012-06-08 07:55:51 +0200

    Add DVFS stub for i9300 camera HAL
    The i9300 camera HAL locks/unlock the DVFS while camera is running.
    This is just a stub as any normal governor works fine for camera.
    
    Change-Id: If2a948fa74dcfe0879ebc58f1483a353099b6c5a

--- power/Android.mk
+++ power/Android.mk
@@ -2,6 +2,10 @@
 
 LOCAL_SRC_FILES += power/power.c
 
+ifeq ($(TARGET_SOC),exynos4x12)
+  LOCAL_FLAGS += -DUSE_DVFS_STUB
+endif
+
 ifeq ($(QEMU_HARDWARE),true)
   LOCAL_SRC_FILES += power/power_qemu.c
   LOCAL_CFLAGS    += -DQEMU_POWER=1
