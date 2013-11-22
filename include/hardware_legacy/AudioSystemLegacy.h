e84195eda8e87dae9a677d7ccf26eebd6bcea969
TheWhisp <daavis.90@gmail.com>
2013-11-22 15:37:23 +0200

    audio: add missing audio formats for msm7x27a
    Change-Id: I12952dfe2eb3c87849472bf521703cd438cf4b1f

--- include/hardware_legacy/AudioSystemLegacy.h
+++ include/hardware_legacy/AudioSystemLegacy.h
@@ -150,6 +150,12 @@ public:
         HE_AAC_V1           = 0x05000000,
         HE_AAC_V2           = 0x06000000,
         VORBIS              = 0x07000000,
+#ifdef QCOM_HARDWARE
+        EVRC                = 0x08000000,
+        QCELP               = 0x09000000,
+        EVRCB               = 0x10000000,
+        EVRCWB              = 0x11000000,
+#endif
         MAIN_FORMAT_MASK    = 0xFF000000,
         SUB_FORMAT_MASK     = 0x00FFFFFF,
         // Aliases
