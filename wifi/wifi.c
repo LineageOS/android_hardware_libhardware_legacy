b19b3f5d726edf95d49e7decbb9bd5de118f1788
extremetek <tekgamingxtreme@gmail.com>
2017-03-20 18:36:14 +0700

    Fix bugs

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -55,10 +55,6 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#ifdef SAMSUNG_WIFI
-#include <samsung_macloader.h>
-#endif
-
 extern int do_dhcp();
 extern int ifc_init();
 extern void ifc_close();
